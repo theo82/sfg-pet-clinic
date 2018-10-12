9630bed1f92958c2417ebbf51f06190d055e9621
theo82 <theoaristi53@gmail.com>
2018-10-12 11:45:32 +0300

    Create Owner Index page and controller

---
+++ pet-clinic-web/src/main/java/theo/tziomakas/sfgpetclinic/controllers/OwnerController.java
@@ -0,0 +1,15 @@
+package theo.tziomakas.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"","/","/index.html","/index"})
+    public String listOwners(){
+        return "owners/index";
+    }
+
+}
