7d89310601c050d42afc4c5ac3bcc7ad78f9e124
theo82 <theoaristi53@gmail.com>
2018-10-13 17:29:25 +0300

    List of vets.

--- pet-clinic-web/src/main/java/theo/tziomakas/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/theo/tziomakas/sfgpetclinic/controllers/VetController.java
@@ -1,13 +1,26 @@
 package theo.tziomakas.sfgpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import theo.tziomakas.sfgpetclinic.services.VetService;
+
+import javax.jws.WebParam;
 
 @Controller
 public class VetController {
-    
+
+    VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets","/vets/index.html","vets/index"})
-    public String listVets(){
+    public String listVets(Model model){
+
+        model.addAttribute("vets",vetService.findAll());
+
         return "vets/index";
     }
 }
