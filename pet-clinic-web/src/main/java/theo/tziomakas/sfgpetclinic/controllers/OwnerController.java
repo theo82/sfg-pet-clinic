a36285a42b1a11847c9dee0f6e191be2f70dde06
theo82 <theoaristi53@gmail.com>
2018-10-20 11:04:18 +0300

    Fixing broken links

--- pet-clinic-web/src/main/java/theo/tziomakas/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/theo/tziomakas/sfgpetclinic/controllers/OwnerController.java
@@ -24,4 +24,11 @@ public class OwnerController {
         return "owners/index";
     }
 
+    @RequestMapping("/find")
+    public String findOwners(){
+
+        return "notimplemented";
+
+    }
+
 }
